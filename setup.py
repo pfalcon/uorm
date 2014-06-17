abac0bbb4f1f00950c48838b3d06cfe4d4028562
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-06-17 22:28:41 +0300

    Initial implementation.

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+setup(name='uorm',
+      version='0.1',
+      description="""Very lightweight, memory-efficient (uses generator
+protocol), dependency-free anti-ORM (Object-Relational Mapper) for
+MicroPython. Using this module, one can simplify typical database access
+operations, without trading well-known SQL for an obscure and inefficient
+ORM pseudo-language.
+""",
+      url='https://github.com/pfalcon/uorm',
+      author='Paul Sokolovsky',
+      author_email='pfalcon@users.sourceforge.net',
+      license='MIT',
+      py_modules=['uorm'])
