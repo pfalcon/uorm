081f1b882a071e9acebfb96118fa67d678ed035c
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2019-07-21 09:35:11 +0300

    setup.py: Update for Pycopy.

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import setup
 
 
 setup(name='uorm',
-      version='0.3',
-      description="""Very lightweight, memory-efficient (uses generator
-protocol), dependency-free anti-ORM (Object-Relational Mapper) for
-MicroPython. Using this module, one can simplify typical database access
-operations, without trading well-known SQL for an obscure and inefficient
-ORM pseudo-language.
+      version='0.4',
+      description="""Very lightweight, memory-efficient (uses generator \
+protocol), dependency-free anti-ORM (Object-Relational Mapper) for \
+Pycopy (https://github.com/pfalcon/pycopy). Using this module, one can \
+simplify typical database access operations, without trading well-known \
+SQL for an obscure and inefficient ORM pseudo-language.
 """,
       url='https://github.com/pfalcon/uorm',
       author='Paul Sokolovsky',
       author_email='pfalcon@users.sourceforge.net',
       license='MIT',
       py_modules=['uorm'],
-      install_requires=['micropython-sqlite3'])
+      install_requires=['pycopy-sqlite3'])
